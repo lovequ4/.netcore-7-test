a64bb5da3a69d53c999612496fb488e0b6dad33f
Sam <a0983131803@gmail.com>
2023-11-13 12:21:23 +0800

    update

--- Data/ApiDbContext.cs
+++ Data/ApiDbContext.cs
@@ -29,4 +29,9 @@ public partial class ApiDbContext : IdentityDbContext<IdentityUser>
     }
 
     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; } 
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<Order> Orders { get; set; }
 }
