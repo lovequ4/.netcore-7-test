1654a0320947465e8c5092cbbedafe422dad89df
Sam <a0983131803@gmail.com>
2023-11-13 12:24:55 +0800

    Categories CRUD

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using CartWebApi;
+using System.ComponentModel.DataAnnotations;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string Slug { get; set; }
+}
